aa2d0e55dcc3cd9646a0d7b528c83c265f81c49d
adbi-hk <biaddiaelhak@gmail.com>
2024-12-03 22:50:15 +0100

    1:10 after i created the navbar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,14 +1,28 @@
-import React from "react";
+import MobileNav from "../../components/MobileNav";
+import Sidebar from "../../components/Sidebar";
+import Image from "next/image";
+
+
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const LoggedIn = { firstName :"Adrian" , lastName:"JSM"};
   return (
-    <main>
-        SIDEBARE 
-        {children}
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user = {LoggedIn}/>
+
+        <div className="flex size-full flex-col ">
+          <div className="root-layout">
+            <Image src={"/icons/logo.svg"} width={30} height={30} alt="logo" />
+            <div>
+              <MobileNav user ={LoggedIn}/>
+            </div>
+          </div>
+          {children}
+        </div>
     </main>
      );
 }
---
+++ src/components/MobileNav.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+  
+const MobileNav = ({user}:MobileNavProps) => {
+  return (
+    <section>
+
+    </section>
+  )
+}
+
+export default MobileNav
